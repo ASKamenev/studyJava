a6b752fe1cb7cfa5e16748526b5376b4869719fb
Andrew <kamenev.ndv@gmail.com>
2017-02-07 23:24:31 +0300

    first commit

---
+++ Calculator.java
@@ -0,0 +1,58 @@
+
+
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andrew
+ */
+public class Calculator {
+    
+    public static void main(String[] args) {
+        int number1, number2, result = 0;
+        String operation;
+        Scanner sc = new Scanner(System.in);
+     
+        System.out.println("Enter the first number: ");
+         number1 = sc.nextInt();
+         
+         System.out.println("Enter the second number: ");
+         number2 = sc.nextInt();
+         
+        System.out.println("What do you want to do? ");
+        operation = sc.next();
+        
+        while (operation != "+") {
+            
+           System.out.println("again");
+            
+           
+       }
+              
+        switch(operation) {
+           case ("+"):               
+               result = number1 + number2;
+               break;
+           case "-":    
+               result = number1 - number2;
+               break;
+           case "*":    
+               result = number1 * number2;
+               break;
+           case "/":    
+               result = number1 / number2;
+               break;
+        }
+       
+        
+        System.out.println(number1 + operation + number2 + "=" + result);
+    }
+        
+}
