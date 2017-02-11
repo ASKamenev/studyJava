4bd9dffb69849d2eac5436b36156cf5f2d1c6e68
Andrew <kamenev.ndv@gmail.com>
2017-02-11 18:23:03 +0300

    calc v 0.3

---
+++ Calculator/CheckInput.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+import static Calculator.Operations.*;
+
+/**
+ *
+ * @author andrew
+ */
+public class CheckInput {
+    
+     public static void choseOperation(String operation) {
+        switch(operation) {
+            case "+":
+                Operations.calcFold(number1, number2);
+            case "-":
+                Operations.calcDeduct(number1, number2);
+            case "*":
+                Operations.calcMultiply(number1, number2);
+            case "/":
+                Operations.calcDivide(number1, number2);
+        }
+                
+    }
+      
+    
+}
